61b06604967345bb238f50ec1128e31ceb0774e6
Matty Williams <mw2122@doc.ic.ac.uk>
2024-01-23 13:38:24 +0000

    Implemented tests for parser.scala

--- src/test/wacc/parser.scala
+++ src/test/wacc/parser.scala
@@ -8,37 +8,82 @@ import parsley.expr.chain
 
 class ParserSpec extends AnyFlatSpec {
 
-    "7" should "return 7" in {
+    "The string \"7\"" should "return Success(7)" in {
         info("Make an input string containing \"7\"")
         val input = "7"
         info("Parse the input string")
         val output = parser.parse(input)
+        info("It should be Success(7)")
         output shouldBe Success(7)
     }
 
-    "5 + 6" should "return 11" in {
-        info("Not implemented!")
-        assert(false)
+    "The string \"5 + 6\"" should "return Success(11)" in {
+        info("Make an input string containing \"5 + 6\"")
+        val input = "5 + 6"
+        info("Parse the input string")
+        val output = parser.parse(input)
+        info("It should be Success(11)")
+        output shouldBe Success(11)
     }
 
-    "9 - 2" should "return 7" in {
-        info("Not implemented!")
-        assert(false)
+    "The string \"9 - 2\"" should "return Success(7)" in {
+        info("Make an input string containing \"9 - 2\"")
+        val input = "9 - 2"
+        info("Parse the input string")
+        val output = parser.parse(input)
+        info("It should be Success(7)")
+        output shouldBe Success(7)
     }
 
-    "5 + 6 - 3" should "return 8" in {
-        info("Not implemented!")
-        assert(false)
+    "The string \"5 + 6 - 3\"" should "return Success(8)" in {
+        info("Make an input string containing \"5 + 6 - 3\"")
+        val input = "5 + 6 - 3"
+        info("Parse the input string")
+        val output = parser.parse(input)
+        info("It should be Success(8)")
+        output shouldBe Success(8)
     }
 
-    "9 - 2 + 3" should "return 10" in {
-        info("Not implemented!")
-        assert(false)
+    "The string \"9 - 2 + 3\"" should "return Success(10)" in {
+        info("Make an input string containing \"9 - 2 + 3\"")
+        val input = "9 - 2 + 3"
+        info("Parse the input string")
+        val output = parser.parse(input)
+        info("It should be Success(10)")
+        output shouldBe Success(10)
     }
 
-    "9 - 2 - 3" should "return 4" in {
-        info("Not implemented!")
-        assert(false)
+    "The string \"9 - 2 - 3\"" should "return Success(4)" in {
+        info("Make an input string containing \"9 - 2 - 3\"")
+        val input = "9 - 2 - 3"
+        info("Parse the input string")
+        val output = parser.parse(input)
+        info("It should be Success(4)")
+        output shouldBe Success(4)
+    }
+
+    "The string \"+\"" should "return Failure(_)" in {
+        info("Make an input string containing \"+\"")
+        val input = "+"
+        info("Parse the input string")
+        val output = parser.parse(input)
+        info("It should be Failure(_)")
+        output match {
+            case Failure(msg) => assert(true)
+            case Success(x) => assert(false)
+        }
+    }
+
+    "The string \"g\"" should "return Failure(_)" in {
+        info("Make an input string containing \"g\"")
+        val input = "g"
+        info("Parse the input string")
+        val output = parser.parse(input)
+        info("It should be Failure(_)")
+        output match {
+            case Failure(msg) => assert(true)
+            case Success(x) => assert(false)
+        }
     }
 }
 
