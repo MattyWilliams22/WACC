8e5e73c139eeb4b402dd961a5745eb2cb7beb0f8
Maciej Rytlewski <msr22@ic.ac.uk>
2024-01-25 18:37:53 +0000

    [msr22] Limited integers to 32 bits and listed all descriptions within the Lexical description, in preparation for specifying rules for WACC

--- src/main/scala/wacc/lexer.scala
+++ src/main/scala/wacc/lexer.scala
@@ -6,11 +6,25 @@ import parsley.token.descriptions._
 
 object lexer {
     private val desc = LexicalDesc.plain.copy(
-        // your configuration goes here
+        symbolDesc = SymbolDesc.plain.copy(
+
+        ),
+        nameDesc = NameDesc.plain.copy(
+
+        ),
+        spaceDesc = SpaceDesc.plain.copy(
+
+        ),
+        numericDesc = numeric.NumericDesc.plain.copy(
+
+        ),
+        textDesc = text.TextDesc.plain.copy(
+
+        ),
     )
     private val lexer = new Lexer(desc)
 
-    val integer = lexer.lexeme.integer.decimal
+    val integer = lexer.lexeme.integer.decimal32
     val implicits = lexer.lexeme.symbol.implicits
     def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
 }
