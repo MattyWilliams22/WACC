dfe5b828a1ee8cb6411a8984e9505fdcecd4a5bd
Dina Duong <dina.duong22@imperial.ac.uk>
2024-01-29 00:31:37 +0000

    [dhd22] feat: Added file parsing, kept expression parsing

--- src/main/scala/wacc/Main.scala
+++ src/main/scala/wacc/Main.scala
@@ -1,17 +1,53 @@
 package wacc
 
-import parsley.{Success, Failure}
+import parsley.{Failure, Success}
+
+import java.io.File
+import scala.io.Source
 
 object Main {
-    def main(args: Array[String]): Unit = {
-        println("hello WACC!")
+  private val FILE_ERR_CODE = 150
 
-        args.headOption match {
-            case Some(expr) => parser.parse(expr) match {
-                case Success(x) => println(s"$expr = $x")
-                case Failure(msg) => println(msg)
-            }
-            case None => println("please enter an expression")
-        }
+  def main(args: Array[String]): Unit = {
+    println("hello WACC!")
+
+    if (args.length < 1) {
+      println("please enter an expression")
+      System.exit(FILE_ERR_CODE)
     }
+
+    if (args(0).endsWith(".wacc")) {
+
+      val inputFile = new File(args(0))
+
+      /* Checks if file exists*/
+      if (!inputFile.exists()) {
+        println("File does not exist")
+        System.exit(FILE_ERR_CODE)
+      }
+
+      /* Checks if input string is a file */
+      if (!inputFile.isFile) {
+        println("It's not a file")
+        System.exit(FILE_ERR_CODE)
+      }
+
+      /*Reads file contents */
+      val source = Source.fromFile(inputFile)
+      val fileContents = try source.mkString finally source.close()
+
+      /* Parsing result */
+      val parseResult = parser.parse(fileContents)
+      parseResult match {
+        case Success(x) => println(s"$parseResult = $x")
+        case Failure(msg) => println(msg)
+      }
+    } else {
+      /* Parsing of expression */
+      parser.parse(args(0)) match {
+        case Success(x) => println(s"$args(0) = $x")
+        case Failure(msg) => println(msg)
+      }
+    }
+  }
 }
