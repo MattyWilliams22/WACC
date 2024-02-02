e9da3c54eb62d7c04c5c7c96dc41ed0137a679e3
Maciej Rytlewski <msr22@ic.ac.uk>
2024-02-02 15:41:51 +0000

    [dhd22][msr22] Fact: Reorder the traits and parsing tokens

--- src/main/scala/wacc/ASTNodes.scala
+++ src/main/scala/wacc/ASTNodes.scala
@@ -2,6 +2,38 @@ package wacc
 
 object ASTNodes {
 
+  case class Program(funcs: List[Function], stat: Statement)
+
+  case class Function(_type: Type, ident: Ident, param_list: List[Param], body: Statement)
+
+  case class Param(_type: Type, ident: Ident)
+
+  sealed trait Statement
+  case class Skip() extends Statement
+  case class Declare(_type: Type, ident: Ident, value: RValue) extends Statement
+  case class Assign(lvalue: LValue, rvalue: RValue) extends Statement
+  case class Read(lvalue: LValue) extends Statement
+  case class Action(action: String, exp: Expr) extends Statement
+  case class If(cond: Expr, thenS: Statement, elseS: Statement) extends Statement
+  case class While(cond: Expr, body: Statement) extends Statement
+  case class Scope(body: Statement) extends Statement
+  case class Statements(stmts: List[Statement]) extends Statement
+
+  sealed trait Type
+  sealed trait PairElemT extends Type
+  case class BaseT(str: String) extends Type with PairElemT
+  case class ArrayT(_type: Type, dim: Int) extends Type with PairElemT
+  case class PairT(pet_1: PairElemT, pet_2: PairElemT) extends Type
+  case class PairNull() extends PairElemT
+
+  sealed trait LValue
+
+  sealed trait RValue
+  case class ArrayLiter(elems: List[Expr]) extends RValue
+  case class NewPair(exp1: Expr, exp2: Expr) extends RValue
+  case class PairElem(func: String, lvalue: LValue) extends RValue with LValue
+  case class Call(funcName: Ident, args: List[Expr]) extends RValue
+
   sealed trait Expr extends RValue
   case class Mul(exp1: Expr, exp2: Expr) extends Expr
   case class Div(exp1: Expr, exp2: Expr) extends Expr
@@ -30,37 +62,4 @@ object ASTNodes {
   case class PairLiter(str: String) extends Atom
   case class Ident(str: String) extends Atom with LValue
   case class ArrayElem(ident: Ident, args: List[Expr]) extends Atom with LValue
-
-  sealed trait Type
-  sealed trait PairElemT extends Type
-  case class BaseT(str: String) extends Type with PairElemT
-  case class ArrayT(_type: Type, dim: Int) extends Type with PairElemT
-  case class PairT(pet_1: PairElemT, pet_2: PairElemT) extends Type
-  case class PairNull() extends PairElemT
-
-  case class Prog(funcs: List[Function], stat: Statement)
-
-  case class Function(_type: Type, ident: Ident, param_list: List[Param], body: Statement)
-
-  case class Param(_type: Type, ident: Ident)
-
-  sealed trait Statement
-  case class Skip() extends Statement
-  case class Declare(_type: Type, ident: Ident, value: RValue) extends Statement
-  case class Assign(lvalue: LValue, rvalue: RValue) extends Statement
-  case class Read(lvalue: LValue) extends Statement
-  case class Action(action: String, exp: Expr) extends Statement
-  case class If(cond: Expr, thenS: Statement, elseS: Statement) extends Statement
-  case class While(cond: Expr, body: Statement) extends Statement
-  case class Scope(body: Statement) extends Statement
-  case class Statements(stmts: List[Statement]) extends Statement
-
-  sealed trait LValue
-
-  sealed trait RValue
-  case class ArrayLiter(elems: List[Expr]) extends RValue
-  case class NewPair(exp1: Expr, exp2: Expr) extends RValue
-  case class PairElem(func: String, lvalue: LValue) extends RValue with LValue
-  case class Call(funcName: Ident, args: List[Expr]) extends RValue
-
 }
--- src/main/scala/wacc/parser.scala
+++ src/main/scala/wacc/parser.scala
@@ -10,20 +10,25 @@ import wacc.lexer._
 import wacc.lexer.implicits.implicitSymbol
 
 object parser {
-  def parse(input: String): Result[String, Prog] = parser.parse(input)
+  def parse(input: String): Result[String, Program] = parser.parse(input)
   private [wacc] def parseTest(input: String): Result[String, Expr] = parserTest.parse(input)
 
   private lazy val parserTest = fully(expr)
   private val parser = fully(prog)
 
-  private lazy val prog: Parsley[Prog] =
-    (begin ~> many(atomic(func)) <~> statement <~ end).map(x => Prog(x._1, x._2))
+  private lazy val prog: Parsley[Program] =
+    (begin ~> many(atomic(func)) <~> statement <~ end).map(x => Program(x._1, x._2))
 
   private lazy val func: Parsley[Function] =
     (((types <~> ident <~ "(" <~> option(paramList) <~ ")") <~ is) <~> statement <~ end).map {
       case (((t, i), Some(ps)), s) => Function(t, Ident(i), ps, s)
       case (((t, i), None), s) => Function(t, Ident(i), List(), s)
     }
+
+  private lazy val paramList: Parsley[List[Param]] =
+    (param <~> many("," ~> param)).map(x => x._1 :: x._2)
+
+  private lazy val param: Parsley[Param] = (types <~> ident).map(x => Param(x._1, Ident(x._2)))
 
   private lazy val statement: Parsley[Statement] = {
     ((atomic(skip).map(_ => Skip()) |
@@ -40,10 +45,68 @@ object parser {
       atomic(begin ~> statement <~ end).map(Scope)) <~> many(";" ~> statement)).map(x => Statements(x._1 :: x._2))
   }
 
-  private lazy val paramList: Parsley[List[Param]] =
-    (param <~> many("," ~> param)).map(x => x._1 :: x._2)
+  private lazy val lvalue: Parsley[LValue] = {
+    atomic(arrayElem).map(x => ArrayElem(Ident(x._1), x._2)) |
+      atomic(pairElem).map(x => PairElem(x._1, x._2)) | atomic(ident).map(Ident)
+  }
 
-  private lazy val param: Parsley[Param] = (types <~> ident).map(x => Param(x._1, Ident(x._2)))
+  private lazy val rvalue: Parsley[RValue] = {
+    atomic("newpair(" ~> expr <~ "," <~> expr <~ ")").map(x => NewPair(x._1, x._2)) |
+      atomic((call ~> ident <~ "(") <~> option(sepBy(expr, ",")) <~ ")").map {
+        case (name: String, Some(x: List[Expr])) => Call(Ident(name), x)
+        case (name: String, None) => Call(Ident(name), List())
+      } |
+      atomic(expr) |
+      atomic(pairElem).map(x => PairElem(x._1, x._2)) |
+      ("[" ~> option(expr <+:> many("," ~> expr)) <~ "]").map {
+        case Some(x: Seq[Expr]) => ArrayLiter(x.toList)
+        case None => ArrayLiter(List())
+      }
+  }
+
+  private lazy val arrayElem = ident <~> some("[" ~> expr <~ "]")
+
+  private lazy val pairElem: Parsley[(String, LValue)] = fst <~> lvalue | snd <~> lvalue
+
+  private lazy val ifElse: Parsley[If] = {
+    (((IF ~> expr <~ THEN) <~> statement <~ ELSE) <~> statement <~ fi).map {
+      case ((cond, thenS), elseS) => If(cond, thenS, elseS)
+    }
+  }
+
+  private lazy val declare: Parsley[Declare] = {
+    ((types <~> ident) <~> ("=" ~> rvalue)).map {
+      case ((t, i), r) => Declare(t, Ident(i), r)
+    }
+  }
+
+  private lazy val types: Parsley[Type] = {
+    (pairType <~> many(arrayBraces)).map {
+      case (base: PairT, arr: List[String]) if arr.isEmpty => base
+      case (base: PairT, arr: List[String]) => ArrayT(base, arr.length)
+    } |
+      (baseTypes <~> many(arrayBraces) <~ some(whitespace)).map {
+        case (base: BaseT, arr: List[String]) if arr.isEmpty => base
+        case (base: BaseT, arr: List[String]) => ArrayT(base, arr.length)
+      }
+  }
+
+  private lazy val pairType: Parsley[PairT] =
+    ("pair(" ~> pairElemType <~ "," <~> pairElemType <~ ")").map(x => PairT(x._1, x._2))
+
+  private lazy val pairElemType: Parsley[PairElemT] =
+    atomic(pairType <~> some(arrayBraces)).map {
+      case (base: PairT, arr: List[String]) => ArrayT(base, arr.length)
+    } |
+      atomic("pair").map(_ => PairNull()) |
+      (baseTypes <~> many(arrayBraces)).map {
+        case (base: BaseT, arr: List[String]) if arr.isEmpty => base
+        case (base: BaseT, arr: List[String]) => ArrayT(base, arr.length)
+      }
+
+  private lazy val baseTypes: Parsley[BaseT] = {
+    baseType.map(BaseT)
+  }
 
   private lazy val expr: Parsley[Expr] = {
     precedence(atom)(
@@ -66,69 +129,6 @@ object parser {
       Ops(InfixR)("&&" as And),
       Ops(InfixR)("||" as Or)
     )
-  }
-
-  private lazy val arrayElem = ident <~> some("[" ~> expr <~ "]")
-
-  private lazy val types: Parsley[Type] = {
-    (pairType <~> many(arrayBraces)).map {
-      case (base: PairT, arr: List[String]) if arr.isEmpty => base
-      case (base: PairT, arr: List[String]) => ArrayT(base, arr.length)
-    } |
-      (baseTypes <~> many(arrayBraces) <~ some(whitespace)).map {
-        case (base: BaseT, arr: List[String]) if arr.isEmpty => base
-        case (base: BaseT, arr: List[String]) => ArrayT(base, arr.length)
-      }
-  }
-
-  private lazy val baseTypes: Parsley[BaseT] = {
-    baseType.map(BaseT)
-  }
-
-  private lazy val pairType: Parsley[PairT] =
-    ("pair(" ~> pairElemType <~ "," <~> pairElemType <~ ")").map(x => PairT(x._1, x._2))
-
-  private lazy val pairElemType: Parsley[PairElemT] =
-    atomic(pairType <~> some(arrayBraces)).map {
-      case (base: PairT, arr: List[String]) => ArrayT(base, arr.length)
-    } |
-      atomic("pair").map(_ => PairNull()) |
-      (baseTypes <~> many(arrayBraces)).map {
-        case (base: BaseT, arr: List[String]) if arr.isEmpty => base
-        case (base: BaseT, arr: List[String]) => ArrayT(base, arr.length)
-      }
-
-  private lazy val declare: Parsley[Declare] = {
-    ((types <~> ident) <~> ("=" ~> rvalue)).map {
-      case ((t, i), r) => Declare(t, Ident(i), r)
-    }
-  }
-
-  private lazy val ifElse: Parsley[If] = {
-    (((IF ~> expr <~ THEN) <~> statement <~ ELSE) <~> statement <~ fi).map {
-      case ((cond, thenS), elseS) => If(cond, thenS, elseS)
-    }
-  }
-
-  private lazy val pairElem: Parsley[(String, LValue)] = fst <~> lvalue | snd <~> lvalue
-
-  private lazy val lvalue: Parsley[LValue] = {
-    atomic(arrayElem).map(x => ArrayElem(Ident(x._1), x._2)) |
-      atomic(pairElem).map(x => PairElem(x._1, x._2)) | atomic(ident).map(Ident)
-  }
-
-  private lazy val rvalue: Parsley[RValue] = {
-    atomic("newpair(" ~> expr <~ "," <~> expr <~ ")").map(x => NewPair(x._1, x._2)) |
-      atomic((call ~> ident <~ "(") <~> option(sepBy(expr, ",")) <~ ")").map {
-        case (name: String, Some(x: List[Expr])) => Call(Ident(name), x)
-        case (name: String, None) => Call(Ident(name), List())
-      } |
-      atomic(expr) |
-      atomic(pairElem).map(x => PairElem(x._1, x._2)) |
-      ("[" ~> option(expr <+:> many("," ~> expr)) <~ "]").map {
-        case Some(x: Seq[Expr]) => ArrayLiter(x.toList)
-        case None => ArrayLiter(List())
-      }
   }
 
   private lazy val atom: Parsley[Expr] = {
