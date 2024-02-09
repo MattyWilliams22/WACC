a9ac2c183a89d35bdcdb85e7e48d70270bdaf849
Kritik Pant <kp1222@ic.ac.uk>
2024-02-09 17:03:53 +0000

    [kp1222] Feat: Add more tests for main to check exit codes

--- src/test/wacc/Main.scala
+++ src/test/wacc/Main.scala
@@ -31,5 +31,33 @@ class MainSpec extends AnyFlatSpec with Matchers {
     }
     output should include("5 + 10 = Add(Num(5),Num(10))")
   }
+
+  it should "return syntax error" in {
+    val exception = intercept[SecurityException] {
+      Main.main(Array("begin end"))
+    }
+    exception.getMessage should include("Syntax Error")
+  }
+
+  it should "return file does not exist error" in {
+    val exception = intercept[SecurityException] {
+      Main.main(Array("nonexistent.wacc"))
+    }
+    exception.getMessage should include("File does not exist")
+  }
+
+  it should "return semantic error" in {
+    val exception = intercept[SecurityException] {
+      Main.main(Array("begin string x = 5 end"))
+    }
+    exception.getMessage should include("Semantic Error")
+  }  
+
+  it should "return success code for valid program" in {
+    val exception = intercept[SecurityException] {
+      Main.main(Array("begin int x = 5 end"))
+    }
+    exception.getMessage should include("Program compiled successfully")
+  }
 }
 
