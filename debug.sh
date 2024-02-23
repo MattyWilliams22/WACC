5f7276f79a134fe1d466baf89e9d148297242195
Dina Duong <dhd22@ic.ac.uk>
2024-02-23 13:54:43 +0000

    [dhd22] Feat: added debug script that runs compile, gcc and qemu

---
+++ debug.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get the file name from the command line arguments
+fname=$1
+
+# Strip the file extension from the file name
+filename=$(basename -- "$fname")
+filename="${filename%.*}"
+
+echo "make"
+make
+
+# Print and run the compile command
+echo "./compile $fname"
+./compile $fname
+
+# Print and run the gcc command
+echo "arm-linux-gnueabi-gcc -o execFile -z noexecstack -march=armv6 $filename.s"
+arm-linux-gnueabi-gcc -o execFile -z noexecstack -march=armv6 $filename.s
+
+# Print and run the qemu-arm command
+echo "qemu-arm -L /usr/arm-linux-gnueabi/ execFile"
+qemu-arm -L /usr/arm-linux-gnueabi/ execFile
+
+rm $filename.s
+rm execFile
