a78c0714fa4e9bead27da544ae0ffac3f4b9b1c9
Maciej Rytlewski <msr22@ic.ac.uk>
2024-03-04 16:41:38 +0000

    [msr22] Fix: Properly added Optimiser and Peephole files to git

---
+++ src/main/scala/wacc/extensions/Optimiser.scala
@@ -0,0 +1,10 @@
+package wacc.extensions
+
+import wacc.backend.Instruction
+import scala.collection.mutable.ListBuffer
+
+object Optimiser {
+  def optimiseInstructions(instructions: ListBuffer[Instruction]): ListBuffer[Instruction] = {
+    instructions
+  }
+}
---
+++ src/main/scala/wacc/extensions/Peephole.scala
@@ -0,0 +1,3 @@
+object Peephole {
+
+}
