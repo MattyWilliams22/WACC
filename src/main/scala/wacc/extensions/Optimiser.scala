a39fa2f643ff898ae707e18109e81608f1d7fe5a
Maciej Rytlewski <msr22@ic.ac.uk>
2024-03-05 11:56:57 +0000

    [msr22] Feat: Added some simple optimisations to Peephole, and made it so that optimisations are made continuously until no more are found

--- src/main/scala/wacc/extensions/Optimiser.scala
+++ src/main/scala/wacc/extensions/Optimiser.scala
@@ -1,10 +1,18 @@
 package wacc.extensions
 
 import wacc.backend.Instruction
+import wacc.extensions.Peephole._
 import scala.collection.mutable.ListBuffer
 
 object Optimiser {
   def optimiseInstructions(instructions: ListBuffer[Instruction]): ListBuffer[Instruction] = {
-    instructions
+    var optimisedInstructions = instructions.toList
+    var newOptimisedInstructions = optimise(optimisedInstructions)
+
+    while (optimisedInstructions != newOptimisedInstructions) {
+      optimisedInstructions = newOptimisedInstructions
+      newOptimisedInstructions = optimise(optimisedInstructions)
+    }
+    ListBuffer.from(optimisedInstructions)
   }
 }
--- src/main/scala/wacc/extensions/Peephole.scala
+++ src/main/scala/wacc/extensions/Peephole.scala
@@ -1,3 +1,22 @@
-object Peephole {
+package wacc.extensions
 
+import scala.collection.mutable.ListBuffer
+
+import wacc.backend._
+
+object Peephole {
+  def optimise(instructions: List[Instruction]): List[Instruction] = {
+    instructions match {
+      case Mov(dest, src, _) :: tail if dest == src =>
+        optimise(tail)
+      case AddInstr(_, _, ImmVal(0), _) :: tail =>
+        optimise(tail)
+      case SubInstr(_, _, ImmVal(0), _) :: tail =>
+        optimise(tail)
+      case head :: tail =>
+        head :: optimise(tail)
+      case Nil =>
+        Nil
+    }
+  }
 }
