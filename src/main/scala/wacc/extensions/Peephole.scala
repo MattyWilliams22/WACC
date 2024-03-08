cddde463f9e6a5919e699a028198e49d73864258
Kritik Pant <kp1222@ic.ac.uk>
2024-03-08 21:56:18 +0000

    [kp1222] Feat: Make optimisations cascade until no more optimisations can be applied

--- src/main/scala/wacc/extensions/Peephole.scala
+++ src/main/scala/wacc/extensions/Peephole.scala
@@ -6,28 +6,41 @@ import wacc.backend._
 
 object Peephole {
 
-  type Transformation = (Instruction, List[Instruction]) => (List[Instruction], List[Instruction])
+  type Transformation = (Instruction, List[Instruction]) => (List[Instruction], Boolean)
 
   val transformations: List[Transformation] = List(removeRedundantSubAdd, removeRedundantCmp, removeRedundantMov, combineMovAdd, combineDoubleMov, removeRedundantStrLdr, removeRedundantLdr)
 
   def optimise(instr: Instruction, remaining: List[Instruction], transformations: List[Transformation] = transformations): (List[Instruction], List[Instruction]) = {
-    transformations.foldLeft((List(instr), remaining)) { (current, transform) =>
-      current match {
-        case (Nil, newRemaining) => return (Nil, newRemaining)
-        case _ => transform(current._1.head, current._2)
+    var changed = true
+    var currentInstr = instr
+    var remainingInstrs = remaining
+
+    while (changed) {
+      changed = false
+
+      for (transformation <- transformations) {
+        val (newInstrs, wasChanged) = transformation(currentInstr, remainingInstrs)
+
+        if (wasChanged) {
+          changed = true
+          currentInstr = newInstrs.head
+          remainingInstrs = newInstrs.tail
+        }
       }
     }
+
+    (List(currentInstr), remainingInstrs)
   }
 
   /* Removes instructions of the form
       add r0, r0, 0
       sub r0, r0, 0
   */
-  def removeRedundantSubAdd(instr: Instruction, remaining: List[Instruction]): (List[Instruction], List[Instruction]) = {
+  def removeRedundantSubAdd(instr: Instruction, remaining: List[Instruction]): (List[Instruction], Boolean) = {
     instr match {
-      case AddInstr(dest, src, ImmVal(0), _) if dest == src => (Nil, remaining)
-      case SubInstr(dest, src, ImmVal(0), _) if dest == src => (Nil, remaining)
-      case _ => (List(instr), remaining)
+      case AddInstr(dest, src, ImmVal(0), _) if dest == src => (remaining, true)
+      case SubInstr(dest, src, ImmVal(0), _) if dest == src => (remaining, true)
+      case _ => (instr :: remaining, false)
     }
   }
 
@@ -35,26 +48,26 @@ object Peephole {
       cmp r0, 0
       cmp r0, 0
   */
-  def removeRedundantCmp(instr: Instruction, remaining: List[Instruction]): (List[Instruction], List[Instruction]) = {
+  def removeRedundantCmp(instr: Instruction, remaining: List[Instruction]): (List[Instruction], Boolean) = {
     instr match {
       case CmpInstr(dest, ImmVal(0)) => remaining match {
-        case Nil => (List(instr), remaining)
+        case Nil => (instr :: remaining, false)
         case head :: tail => head match {
-          case CmpInstr(dest, ImmVal(0)) => (Nil, remaining)
-          case _ => (List(instr), remaining)
+          case CmpInstr(dest, ImmVal(0)) => (remaining, true)
+          case _ => (instr :: remaining, false)
         }
       }
-      case _ => (List(instr), remaining)
+      case _ => (instr :: remaining, false)
     }
   }
 
   /* Removes instructions of the form
       mov r0, r0
   */
-  def removeRedundantMov(instr: Instruction, remaining: List[Instruction]): (List[Instruction], List[Instruction]) = {
+  def removeRedundantMov(instr: Instruction, remaining: List[Instruction]): (List[Instruction], Boolean) = {
     instr match {
-      case Mov(dest, src, _) if dest == src => (Nil, remaining)
-      case _ => (List(instr), remaining)
+      case Mov(dest, src, _) if dest == src => (remaining, true)
+      case _ => (instr :: remaining, false)
     }
   }
 
@@ -64,11 +77,11 @@ object Peephole {
       =>
       add r0, r0, #1
   */
-  def combineMovAdd(instr: Instruction, remaining: List[Instruction]): (List[Instruction], List[Instruction]) = {
+  def combineMovAdd(instr: Instruction, remaining: List[Instruction]): (List[Instruction], Boolean) = {
     (instr, getInstruction(remaining)) match {
       case (Mov(dest1, ImmVal(value), _), Some(AddInstr(dest2, op1, op2, updateFlags))) if dest1 == op2 =>
-        (List(AddInstr(dest2, op1, ImmVal(value), updateFlags)), dropInstructions(remaining, 1))
-      case _ => (List(instr), remaining)
+        (AddInstr(dest2, op1, ImmVal(value), updateFlags) :: dropInstructions(remaining, 1), true)
+      case _ => (instr :: remaining, false)
     }
   }
 
@@ -78,11 +91,11 @@ object Peephole {
       =>
       mov r2, r1
   */
-  def combineDoubleMov(instr: Instruction, remaining: List[Instruction]): (List[Instruction], List[Instruction]) = {
+  def combineDoubleMov(instr: Instruction, remaining: List[Instruction]): (List[Instruction], Boolean) = {
     (instr, getInstruction(remaining), getInstruction(remaining, 1)) match {
       case (Comment("Start of identifier ", _), Some(Mov(dest1, src1, cond1)), Some(Mov(dest2, src2, cond2))) if dest1 == src2 && cond1 == cond2 =>
-        (List(Mov(dest2, src1, cond1)), dropInstructions(remaining, 2))
-      case _ => (List(instr), remaining)
+        (Mov(dest2, src1, cond1) :: dropInstructions(remaining, 2), true)
+      case _ => (instr :: remaining, false)
     }
   }
 
@@ -93,13 +106,13 @@ object Peephole {
       ldr r0, [r1]
       str r0, [r1]
   */
-  def removeRedundantStrLdr(instr: Instruction, remaining: List[Instruction]): (List[Instruction], List[Instruction]) = {
+  def removeRedundantStrLdr(instr: Instruction, remaining: List[Instruction]): (List[Instruction], Boolean) = {
     (instr, getInstruction(remaining)) match {
       case (instr1@StrInstr(dest1, op1, _), Some(Ldr(dest2, op2))) if dest1 == dest2 && op1 == op2 =>
-        (List(instr1), dropInstructions(remaining, 1))
+        (instr1 :: dropInstructions(remaining, 1), true)
       case (instr1@Ldr(dest1, op1), Some(StrInstr(dest2, op2, _))) if dest1 == dest2 && op1 == op2 =>
-        (List(instr1), dropInstructions(remaining, 1))
-      case _ => (List(instr), remaining)
+        (instr1 :: dropInstructions(remaining, 1), true)
+      case _ => (instr :: remaining, false)
     }
   }
 
@@ -109,11 +122,11 @@ object Peephole {
       =>
       ldr r0, [r2]
   */
-  def removeRedundantLdr(instr: Instruction, remaining: List[Instruction]): (List[Instruction], List[Instruction]) = {
+  def removeRedundantLdr(instr: Instruction, remaining: List[Instruction]): (List[Instruction], Boolean) = {
     (instr, getInstruction(remaining)) match {
       case (Ldr(dest1, op1), Some(Ldr(dest2, op2))) if dest1 == dest2 =>
-        (List(Ldr(dest1, op2)), dropInstructions(remaining, 1))
-      case _ => (List(instr), remaining)
+        (Ldr(dest1, op2) :: dropInstructions(remaining, 1), true)
+      case _ => (instr :: remaining, false)
     }
   }
 
@@ -122,6 +135,8 @@ object Peephole {
     remaining match {
       case Nil => None
       case Comment(_, _) :: tail => getInstruction(tail, n)
+      case head :: tail if n > 0 => getInstruction(tail, n - 1)
+      case head :: tail if n > 0 => getInstruction(tail, n - 1)
       case head :: tail if n > 0 => getInstruction(tail, n - 1)
       case head :: _ => Some(head)
     }
