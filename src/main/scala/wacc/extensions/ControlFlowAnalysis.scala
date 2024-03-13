cc627e3a6dabae90043b81d20fb3e59ab85fbcb9
Matty Williams <mw2122@doc.ic.ac.uk>
2024-03-13 00:33:40 +0000

    [mw2122] Feat: Add tests for control flow and reduce code duplication

--- src/main/scala/wacc/extensions/ControlFlowAnalysis.scala
+++ src/main/scala/wacc/extensions/ControlFlowAnalysis.scala
@@ -19,17 +19,16 @@ object ControlFlowAnalysis {
   private def analyseStatement(stmt: Statement): Statement = {
     stmt match {
       case If(cond, thenS, elseS) =>
-      cond match {
+      analyseExpr(cond) match {
         case Bool("true") => analyseStatement(thenS)
         case Bool("false") => analyseStatement(elseS)
-        case _ => If(cond, analyseStatement(thenS), analyseStatement(elseS))
+        case _ => If(analyseExpr(cond), analyseStatement(thenS), analyseStatement(elseS))
       }
       
       case While(cond, stmt) =>
-      cond match {
-        case Bool("true") => While(cond, analyseStatement(stmt))
+      analyseExpr(cond) match {
         case Bool("false") => Skip()
-        case _ => While(cond, analyseStatement(stmt))
+        case _ => While(analyseExpr(cond), analyseStatement(stmt))
       }
       
       case Statements(stmts) =>
@@ -41,31 +40,36 @@ object ControlFlowAnalysis {
   
   private def analyseExpr(expr: Expr): Expr = {
     expr match {
-      case And(Bool("true"), Bool("true")) => Bool("true")
-      case And(Bool("true"), Bool("false")) => Bool("false")
-      case And(Bool("false"), Bool("true")) => Bool("false")
-      case And(Bool("false"), Bool("false")) => Bool("false")
-      case And(exp1, exp2) => And(analyseExpr(exp1), analyseExpr(exp2))
-      case Or(Bool("true"), Bool("true")) => Bool("true")
-      case Or(Bool("true"), Bool("false")) => Bool("true")
-      case Or(Bool("false"), Bool("true")) => Bool("true")
-      case Or(Bool("false"), Bool("false")) => Bool("false")
-      case Or(exp1, exp2) => Or(analyseExpr(exp1), analyseExpr(exp2))
-      case Not(Bool("true")) => Bool("false")
-      case Not(Bool("false")) => Bool("true")
-      case Not(exp) => Not(analyseExpr(exp))
+      case And(exp1, exp2) =>
+        analyseExpr(exp1) match {
+          case Bool("true") => analyseExpr(exp2)
+          case Bool("false") => Bool("false")
+          case _ => And(analyseExpr(exp1), analyseExpr(exp2))
+        }
+      case Or(exp1, exp2) =>
+        analyseExpr(exp1) match {
+          case Bool("true") => Bool("true")
+          case Bool("false") => analyseExpr(exp2)
+          case _ => Or(analyseExpr(exp1), analyseExpr(exp2))
+        }
+      case Not(exp) =>
+        analyseExpr(exp) match {
+          case Bool("true") => Bool("false")
+          case Bool("false") => Bool("true")
+          case _ => Not(analyseExpr(exp))
+        }
       case EQ(exp1, exp2) => 
-      if (exp1 == exp2) {
-        Bool("true")
-      } else {
-        Bool("false")
-      }
+        if (analyseExpr(exp1) == analyseExpr(exp2)) {
+          Bool("true")
+        } else {
+          EQ(analyseExpr(exp1), analyseExpr(exp2))
+        }
       case NEQ(exp1, exp2) => 
-      if (exp1 != exp2) {
-        Bool("true")
-      } else {
-        Bool("false")
-      }
+        if (analyseExpr(exp1) == analyseExpr(exp2)) {
+          Bool("false")
+        } else {
+          NEQ(analyseExpr(exp1), analyseExpr(exp2))
+        }
       case LT(exp1, exp2) => analyseLT(analyseExpr(exp1), analyseExpr(exp2))
       case GT(exp1, exp2) => analyseGT(analyseExpr(exp1), analyseExpr(exp2))
       case LTEQ(exp1, exp2) => analyseLTEQ(analyseExpr(exp1), analyseExpr(exp2))
@@ -73,76 +77,50 @@ object ControlFlowAnalysis {
       case _ => expr
     }
   }
-  
-  private def analyseLT(exp1: Expr, exp2: Expr): Expr = {
-    (exp1, exp2) match {
+
+  private def analyseComparison(exp1: Expr, exp2: Expr, comparison: (Int, Int) => Boolean): Option[Expr] = {
+    (analyseExpr(exp1), analyseExpr(exp2)) match {
       case (Ch(c1), Ch(c2)) => 
-      if (c1 < c2) {
-        Bool("true")
-      } else {
-        Bool("false")
-      }
+        if (comparison(c1.toInt, c2.toInt)) {
+          Some(Bool("true"))
+        } else {
+          Some(Bool("false"))
+        }
       case (Num(n1), Num(n2)) => 
-      if (n1 < n2) {
-        Bool("true")
-      } else {
-        Bool("false")
-      }
-      case _ => LT(exp1, exp2)
+        if (comparison(n1, n2)) {
+          Some(Bool("true"))
+        } else {
+          Some(Bool("false"))
+        }
+      case _ => None
+    }
+  }
+
+  private def analyseLT(exp1: Expr, exp2: Expr): Expr = {
+    analyseComparison(exp1, exp2, _ < _) match {
+      case Some(expr) => expr
+      case None => LT(analyseExpr(exp1), analyseExpr(exp2))
+    }
+  }
+
+  private def analyseLTEQ(exp1: Expr, exp2: Expr): Expr = {
+    analyseComparison(exp1, exp2, _ <= _) match {
+      case Some(expr) => expr
+      case None => LTEQ(analyseExpr(exp1), analyseExpr(exp2))
     }
   }
 
   private def analyseGT(exp1: Expr, exp2: Expr): Expr = {
-    (exp1, exp2) match {
-      case (Ch(c1), Ch(c2)) => 
-      if (c1 > c2) {
-        Bool("true")
-      } else {
-        Bool("false")
-      }
-      case (Num(n1), Num(n2)) => 
-      if (n1 > n2) {
-        Bool("true")
-      } else {
-        Bool("false")
-      }
-      case _ => GT(exp1, exp2)
+    analyseComparison(exp1, exp2, _ > _) match {
+      case Some(expr) => expr
+      case None => GT(analyseExpr(exp1), analyseExpr(exp2))
     }
   }
-  
-  private def analyseLTEQ(exp1: Expr, exp2: Expr): Expr = {
-    (exp1, exp2) match {
-      case (Ch(c1), Ch(c2)) => 
-      if (c1 <= c2) {
-        Bool("true")
-      } else {
-        Bool("false")
-      }
-      case (Num(n1), Num(n2)) => 
-      if (n1 <= n2) {
-        Bool("true")
-      } else {
-        Bool("false")
-      }
-      case _ => LTEQ(exp1, exp2)
-    }
-  }
-  
+
   private def analyseGTEQ(exp1: Expr, exp2: Expr): Expr = {
-    (exp1, exp2) match {
-      case (Ch(c1), Ch(c2)) => 
-      if (c1 >= c2) {
-        Bool("true")
-      } else {
-        Bool("false")
-      }
-      case (Num(n1), Num(n2)) => 
-      if (n1 >= n2) {
-        Bool("true")
-      } else {
-        Bool("false")
-      }
-      case _ => GTEQ(exp1, exp2)
+    analyseComparison(exp1, exp2, _ >= _) match {
+      case Some(expr) => expr
+      case None => GTEQ(analyseExpr(exp1), analyseExpr(exp2))
     }
   }
   
